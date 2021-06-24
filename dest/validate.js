fe4b76e0bfdf69987cd381c0ea50c4cb05b4acca
namthanhtran <namthanhtran16@gmail.com>
2021-06-24 19:52:02 +0700

    complete validate form js

--- dest/validate.js
+++ dest/validate.js
@@ -2,13 +2,30 @@ function Validator(options) {
 
     var formElement = document.querySelector(options.form);
 
+
+    function Validate(inputElement, rule) {
+        var errorMessage = rule.test(inputElement.value);
+        var errorElement = inputElement.parentElement.querySelector('.form-message');
+
+        if (errorMessage) {
+            errorElement.innerText = errorMessage;
+        } else {
+            errorElement.innerText = '';
+        }
+    }
+
     if (formElement) {
         options.rules.forEach(function(rule) {
+
             var inputElement = formElement.querySelector(rule.selector);
-            console.log(inputElement);
+            var errorElement = inputElement.parentElement.querySelector('.form-message');
+
             if (inputElement) {
                 inputElement.onblur = function() {
-                    if (inputElement.value == '')
+                    Validate(inputElement, rule);
+                }
+                inputElement.oninput = function() {
+                    errorElement.innerText = '';
                 }
             }
         })
@@ -19,7 +36,7 @@ Validator.isRequired = function(selector) {
     return {
         selector: selector,
         test: function(value) {
-            return value.trim() ? undefined : 'vui lòng nhập trường này!'
+            return value.trim() ? undefined : 'Vui lòng nhập trường này !'
         }
     }
 }
@@ -28,7 +45,8 @@ Validator.isEmail = function(selector) {
     return {
         selector: selector,
         test: function(value) {
-            return value.trim() ? undefined : 'vui lòng nhập trường này!'
+            var reg = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+            return reg.test(value) ? undefined : 'Trường này phải là email !'
         }
     }
 }
